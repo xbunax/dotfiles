3c99042a40981b803b5c0bd648679d84a4fde8fe
xbunax <1171218417@qq.com>
2024-01-14 15:14:38 +0800

    add autorunswift

--- nvim/lua/autorun.lua
+++ nvim/lua/autorun.lua
@@ -1,13 +1,35 @@
--- pythons
 local Terminal   = require('toggleterm.terminal').Terminal
+
+-- pythons
 local _runPython = Terminal:new({
     cmd = "/Users/xbunax/anaconda3/bin/python " .. vim.fn.expand("%"),
     -- hidden = false,
     close_on_exit = false,
 })
 
-
 local function runPython()
     _runPython:toggle()
 end
-vim.keymap.set("n", "<F5>", runPython)
+vim.api.nvim_create_autocmd("FileType",{
+    pattern = "python",
+    callback = function()
+        vim.keymap.set("n", "<F5>", runPython)
+    end
+})
+
+-- swift
+local _runSwift = Terminal:new({
+    cmd = "/usr/bin/swift " .. vim.fn.expand("%"),
+    -- hidden = false,
+    close_on_exit = false,
+})
+local function runSwift()
+    _runSwift:toggle()
+end
+
+vim.api.nvim_create_autocmd("FileType",{
+    pattern = "swift",
+    callback = function()
+        vim.keymap.set("n", "<F5>", runSwift)
+    end
+})
