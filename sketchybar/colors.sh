c22c207c3839be4ea5f335a5e99568f060220506
xbunax <1171218417@qq.com>
2024-03-22 13:28:38 +0800

    change wifi command

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -13,14 +13,15 @@ export GREY=0xff939ab7
 export TRANSPARENT=0x00000000
 
 # General bar colors
-export BAR_COLOR=0xff1e1e2e
+export BAR_COLOR=0xBFffff
+# export BAR_COLOR=0xff1e1e2e
 export BAR_BORDER_COLOR=0xff494d64 #0xa024273a
 export ICON_COLOR=$WHITE # Color of all icons
 export LABEL_COLOR=$WHITE # Color of all labels
 export BACKGROUND_1=0x603c3e4f
 export BACKGROUND_2=0x60494d64
 
-# export POPUP_BACKGROUND_COLOR=0x00000000
+# export POPUP_BACKGROUND_COLOR=0xBFfff
 export POPUP_BACKGROUND_COLOR=0xff1e1e2e
 export POPUP_BORDER_COLOR=$WHITE
 
--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -2,7 +2,9 @@
 
 update() {
   source "$CONFIG_DIR/icons.sh"
-  INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: '  '/ SSID: / {print $2}')"
+  # INFO=$(sudo wdutil info | awk -F ': ' '/ SSID/{print $2}')
+  INFO=$(networksetup -getairportnetwork en0 | awk -F': ' '{print $2}')
+  # INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: '  '/ SSID: / {print $2}')"
   LABEL="$INFO ($(ipconfig getifaddr en0))"
   ICON="$([ -n "$INFO" ] && echo "$WIFI_CONNECTED" || echo "$WIFI_DISCONNECTED")"
 
