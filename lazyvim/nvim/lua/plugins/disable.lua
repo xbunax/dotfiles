9fef275eb9125d79bd3807ab4c50d533e8dcfd43
xbunax <1171218417@qq.com>
2024-04-29 09:26:39 +0800

    add lazyvim config

--- lazyvim/nvim/lua/plugins/disable.lua
+++ lazyvim/nvim/lua/plugins/disable.lua
@@ -1,6 +1,8 @@
 return {
-    -- disable trouble
-    { "rcarriga/nvim-notify",        enabled = false },
-    { "lewis6991/gitsigns.nvim",     enabled = false },
-    { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+	-- disable trouble
+	{ "rcarriga/nvim-notify",        enabled = false },
+	{ "lewis6991/gitsigns.nvim",     enabled = false },
+	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
+	{ 'fgheng/winbar.nvim',          enable = false },
+	{ 'akinsho/bufferline.nvim',     enable = false },
 }
