9eda945284bd0cf33739f5e1e8037bf0f4f12a26
xbunax <1171218417@qq.com>
2024-10-07 18:00:54 +0800

    bug fix :change mini comment to comment nvim to get typst comment

--- lazyvim/nvim/lua/plugins/disable.lua
+++ lazyvim/nvim/lua/plugins/disable.lua
@@ -3,8 +3,9 @@ return {
   { "rcarriga/nvim-notify", enabled = false },
   { "lewis6991/gitsigns.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "fgheng/winbar.nvim", enable = false },
-  { "akinsho/bufferline.nvim", enable = false },
+  { "fgheng/winbar.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "echasnovski/mini.comment", enabled = false },
   -- { "kevinhwang91/nvim-ufo", enable = false },
   -- { "karb94/neoscroll.nvim", enable = false },
 }
