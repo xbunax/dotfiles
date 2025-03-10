08e7e8e7be9afdf0b2b2251011636c45a1dc9eb1
july <july@zhumodeMacBook-Air.local>
2025-03-10 13:38:41 +0800

    optmizie sketchybar cava script

--- sketchybar/items/media_cava.lua
+++ sketchybar/items/media_cava.lua
@@ -49,6 +49,55 @@ M.cava = sbar.add("item", "media_cava", {
 	script = "~/.config/sketchybar/helpers/cava.sh",
 })
 
+M.media_artist = sbar.add("item", {
+	position = "left",
+	drawing = false,
+	padding_left = 3,
+	padding_right = 0,
+	width = 0,
+	icon = { drawing = false },
+	label = {
+		width = 0,
+		font = { size = 9 },
+		-- color = colors.with_alpha(colors.white, 0.6),
+		color = colors.orange,
+		max_chars = 18,
+		y_offset = 6,
+	},
+})
+
+M.media_title = sbar.add("item", {
+	position = "left",
+	drawing = false,
+	padding_left = 3,
+	padding_right = 0,
+	icon = { drawing = false },
+	label = {
+		-- color = colors.with_alpha(colors.white, 0.6),
+		color = colors.orange,
+		font = { size = 11 },
+		width = 0,
+		max_chars = 16,
+		y_offset = -5,
+	},
+})
+
+local inter = 0
+local function animate_detail(detail)
+	if not detail then
+		inter = inter - 1
+	end
+	if inter > 0 and not detail then
+		return
+	end
+	print(inter)
+	sbar.animate("tanh", 30, function()
+		M.media_artist:set({ label = { width = detail and "dynamic" or 0 } })
+		M.media_title:set({ label = { width = detail and "dynamic" or 0 } })
+		return
+	end)
+end
+
 sbar.add("item", {
 	position = "popup." .. M.media_cover.name,
 	icon = { string = icons.media.back },
@@ -68,7 +117,7 @@ sbar.add("item", {
 	click_script = "nowplaying-cli next",
 })
 
-M.cava_bracket = sbar.add("bracket", { M.media_cover.name, M.cava.name }, {
+M.cava_bracket = sbar.add("bracket", { M.media_cover.name, M.media_artist.name, M.media_title.name, M.cava.name }, {
 	background = {
 		color = colors.bg3,
 		drawing = true,
@@ -76,18 +125,33 @@ M.cava_bracket = sbar.add("bracket", { M.media_cover.name, M.cava.name }, {
 })
 
 M.cava_watcher:subscribe({ "media_change" }, function(env)
-	local drawing = (env.INFO.state == "playing")
-	if drawing and interupt == 1 then
-		-- M.media_title:set({ drawing = drawing, label = env.INFO.title })
-		-- M.media_artist:set({ drawing = drawing, label = env.INFO.artist })
-		M.media_cover:set({ drawing = drawing })
-		M.cava:set({
-			label = { drawing = drawing },
-			drawing = drawing,
-		})
-		M.cava_bracket:set({
-			drawing = drawing,
-		})
+	if whitelist[env.INFO.app] then
+		local drawing = (env.INFO.state == "playing")
+		if drawing or interupt == 1 then
+			sbar.animate("tanh", 30, function()
+				-- M.media_title:set({ drawing = drawing, label = env.INFO.title })
+				-- M.media_artist:set({ drawing = drawing, label = env.INFO.artist })
+				M.media_cover:set({ drawing = drawing })
+				M.cava:set({
+					label = { drawing = drawing },
+					drawing = drawing,
+				})
+				M.cava_bracket:set({
+					drawing = drawing,
+				})
+			end)
+		end
+		-- else
+		-- 	M.media_title:set({ drawing = drawing, label = env.INFO.title })
+		-- 	M.media_artist:set({ drawing = drawing, label = env.INFO.artist })
+		-- 	M.media_cover:set({ drawing = drawing })
+		-- 	if drawing then
+		-- 		animate_detail(true)
+		-- 		inter = inter + 1
+		-- 		sbar.delay(5, animate_detail)
+		-- 	else
+		-- 		M.media_cover:set({ popup = { drawing = false } })
+		-- 	end
 	end
 end)
 
@@ -115,4 +179,13 @@ end)
 M.media_cover:subscribe("mouse.exited.global", function(env)
 	M.media_cover:set({ popup = { drawing = false } })
 end)
+
+-- M.media_cover:subscribe("mouse.entered", function(env)
+-- 	inter = inter + 1
+-- 	animate_detail(true)
+-- end)
+--
+-- M.media_cover:subscribe("mouse.exited", function(env)
+-- 	animate_detail(false)
+-- end)
 return M
